6fba62efcefaff5caa5819dad9eca2e57e2c524e
Kesin <Kesin@www2192ui.sakura.ne.jp>
2014-04-04 00:42:30 +0900

    Add setup script and put centos specify ls alias

---
+++ setup.sh
@@ -0,0 +1,12 @@
+cd ~/dotfiles
+git submodule init
+git submodule update
+
+cd ~/
+ln -is ~/dotfiles/.bash_profile .
+ln -is ~/dotfiles/.bashrc .
+ln -is ~/dotfiles/.vim .
+ln -is ~/dotfiles/.vimrc .
+ln -is ~/dotfiles/.jshintrc .
+ln -is ~/dotfiles/.screenrc .
+ln -is ~/dotfiles/.tmux.conf .
