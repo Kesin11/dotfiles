d7c06f7458f1cc05ece04f778552543357e855ae
Kenta Kase <kesin1202000@gmail.com>
2017-10-23 16:25:10 +0900

    NeoBundleにまつわる問題の修正、最近使っていないものをsetupから削除

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+# make synbolic link .vim first, because neobundle will install to .vim
+cd ~/
+ln -is ~/dotfiles/.vim .
+
+# NeoBundle
 cd ~/dotfiles
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh ./install.sh
@@ -6,10 +11,9 @@ rm ./install.sh
 cd ~/
 ln -is ~/dotfiles/.bash_profile .
 ln -is ~/dotfiles/.bashrc .
-ln -is ~/dotfiles/.vim .
 ln -is ~/dotfiles/.vimrc .
-ln -is ~/dotfiles/.jshintrc .
-ln -is ~/dotfiles/.screenrc .
+# ln -is ~/dotfiles/.jshintrc .
+# ln -is ~/dotfiles/.screenrc .
 ln -is ~/dotfiles/.tmux.conf .
 ln -is ~/dotfiles/.perltidyrc .
 ln -is ~/dotfiles/.inputrc .
@@ -21,3 +25,5 @@ sh ~/dotfiles/.atom/setup.sh
 # tool
 mkdir ~/dotfiles/tool
 sh ~/dotfiles/tool/setup.sh
+
+echo "Replace .gitconfig to local one if you need use another one"
