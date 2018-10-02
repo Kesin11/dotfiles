fd0dd5292efea5556025bfca814c8d2c4f78f944
Kenta Kase <kesin1202000@gmail.com>
2018-10-02 21:56:37 +0900

    Remove jshint and perltidy rcfiles

--- setup.sh
+++ setup.sh
@@ -12,10 +12,8 @@ cd ~/
 ln -is ~/dotfiles/.bash_profile .
 ln -is ~/dotfiles/.bashrc .
 ln -is ~/dotfiles/.vimrc .
-# ln -is ~/dotfiles/.jshintrc .
 # ln -is ~/dotfiles/.screenrc .
 ln -is ~/dotfiles/.tmux.conf .
-ln -is ~/dotfiles/.perltidyrc .
 ln -is ~/dotfiles/.inputrc .
 ln -is ~/dotfiles/.gitconfig .
 
