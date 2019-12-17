13c65b13a869cdb851bd796e2b3f91fd26ac110b
Kenta Kase <kesin1202000@gmail.com>
2019-12-17 23:26:17 +0900

    Install more common tool using brew cask

--- brew-install.sh
+++ brew-install.sh
@@ -4,7 +4,19 @@ if ! which brew >& /dev/null;then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+brew update
+
 brew install bash-completion
 brew install git
-brew install rbenv
+brew install hub
+brew install jq
 brew install vim
+brew install rbenv
+brew install nvm
+brew install circleci
+brew cask install visual-studio-code
+brew cask install iterm2
+brew cask install boostnote
+brew cask install google-cloud-sdk
+brew cask install docker
+brew cask install google-japanese-ime
