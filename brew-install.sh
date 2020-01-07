caf9340a5a5fa13df69c8ba1effd9ef827fe56f7
Kenta Kase <kesin1202000@gmail.com>
2020-01-07 21:30:03 +0900

    Add brew cask app

--- brew-install.sh
+++ brew-install.sh
@@ -6,6 +6,7 @@ fi
 
 brew update
 
+# CLI
 brew install bash-completion
 brew install git
 brew install hub
@@ -14,9 +15,18 @@ brew install vim
 brew install rbenv
 brew install nvm
 brew install circleci
+brew cask install google-cloud-sdk
+
+# Dev app
 brew cask install visual-studio-code
 brew cask install iterm2
-brew cask install boostnote
-brew cask install google-cloud-sdk
 brew cask install docker
+
+# Utility app
+brew cask install boostnote
+brew cask install bettertouchtool
+brew cask install jasper
+
+# IME, fonts
 brew cask install google-japanese-ime
+brew cask install homebrew/cask-fonts/font-ricty-diminished
