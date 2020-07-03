08617cb952817883199d7d4f676b9cb2a5bc15f0
Kenta Kase <kesin1202000@gmail.com>
2020-07-03 23:39:18 +0900

    Change timezone in devcontainer

--- install_vscode_container.sh
+++ install_vscode_container.sh
@@ -9,5 +9,7 @@ ln -fs ~/dotfiles/.inputrc .
 ln -fs ~/dotfiles/.bashrc .
 ln -fs ~/dotfiles/.bashrc_linux .bashrc_local
 
+# Change timezone
+ln -sf  /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 
 echo "-----Finish!!------"
