c6a3fec32ff1d5a008c1208ce1a83046e2d39f45
Kenta Kase <kesin1202000@gmail.com>
2020-09-19 18:36:40 +0900

    Bugfix set timezone when no root user

--- install.sh
+++ install.sh
@@ -14,6 +14,10 @@ ln -fs ~/dotfiles/.bashrc_linux .bashrc_local
 sh ~/dotfiles/install-git-diff-highlight.sh
 
 # Change timezone
-ln -sf  /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+if [ `whoami` = "root" ]; then
+  ln -sf  /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+else
+  export TZ="Asia/Tokyo"
+fi
 
 echo "-----Finish!!------"
