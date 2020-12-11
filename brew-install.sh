d0a0fce4af48bfab243a4855c0041cea79f6278c
Kenta Kase <kesin1202000@gmail.com>
2020-12-11 21:56:31 +0900

    brew: Update installing homebrew command

--- brew-install.sh
+++ brew-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if ! which brew >& /dev/null;then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew update
