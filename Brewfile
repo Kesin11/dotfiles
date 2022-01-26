1aba32806fb85b6e822abd341adaf032e76c8447
Kenta Kase <kesin1202000@gmail.com>
2022-01-26 21:10:56 +0900

    brew: Add Brewfile

---
+++ Brewfile
@@ -0,0 +1,45 @@
+# CLI
+# この2つが相性悪い？それとも単に順番の問題か
+brew "git"
+brew "bash-completion"
+brew "git-lfs"
+brew "gh"
+brew "jq"
+brew "vim"
+brew "rbenv"
+brew "nvm"
+brew "circleci"
+brew "ncdu"
+brew "pstree"
+brew "shellcheck"
+brew "git-delta"
+brew "yq"
+brew "tfenv"
+brew "kustomize"
+
+# Dev app
+cask "visual-studio-code"
+cask "iterm2"
+cask "google-cloud-sdk"
+brew "robotsandpencils/made/xcodes"
+brew "awscli" 
+# cask "docker"
+
+# Utility app
+cask "boostnote"
+cask "bettertouchtool"
+cask "jasper"
+cask "google-chrome"
+cask "google-chrome-canary"
+cask "zoomus"
+
+# IME, fonts
+cask "google-japanese-ime"
+cask "homebrew/cask-fonts/font-ricty-diminished"
+
+# Additional
+# cask "android-studio"
+# cask "vagrant"
+# cask "virtualbox"
+# cask "virtualbox-extension-pack"
+# cask "multipass"
--- brew-install.sh
+++ brew-install.sh
@@ -1,52 +1,21 @@
 #!/bin/sh
 
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
 if ! which brew >& /dev/null;then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+if [ "$(uname)" == "Linux" ]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  sudo apt-get update
+  sudo apt-get install -y --no-install-recommends build-essential
+fi
+
 brew update
-
-# CLI
-brew install bash-completion
-brew install git
-brew install git-lfs
-brew install hub
-brew install gh
-brew install jq
-brew install vim
-brew install rbenv
-brew install nvm
-brew install circleci
-brew install peco
-brew install google-cloud-sdk
-brew install robotsandpencils/made/xcodes
-brew install ncdu
-brew install pstree
-brew install shellcheck
-brew install delta
-
-# Dev app
-brew install visual-studio-code
-brew install iterm2
-brew install --cask docker
-
-# Utility app
-brew install boostnote
-brew install bettertouchtool
-brew install jasper
-brew install google-chrome
-brew install google-chrome-canary
-brew install zoomus
-
-# IME, fonts
-brew install google-japanese-ime
-brew install homebrew/cask-fonts/font-ricty-diminished
+cd $script_dir
+brew bundle
+brew doctor
 
 # Add symlink
-ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
-
-# Additional
-# brew install android-studio
-# brew install vagrant
-# brew install virtualbox
-# brew install virtualbox-extension-pack
+# ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
