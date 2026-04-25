ed2b8b269bbb35c6e3b5ff103d98c6f50d58807f
Kenta Kase <kesin1202000@gmail.com>
2026-04-25 22:39:04 +0900

    codespaces: drop aqua setup

--- install.sh
+++ install.sh
@@ -41,14 +41,8 @@ if [[ "${IS_CODESPACES:-}" == "1" ]]; then
    curl https://mise.run | sh
    echo "eval \"\$(/home/codespace/.local/bin/mise activate bash)\"" >> ~/.bashrc
 
-   # https://aquaproj.github.io/docs/products/aqua-installer#shell-script
-   curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.1.2/aqua-installer | bash
-   echo 'export PATH=${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH' >> ~/.bashrc
-   echo 'export AQUA_GLOBAL_CONFIG=${HOME}/aqua.yaml' >> ~/.bashrc
-
-   # Install tools from aqua.yaml with aqua global install
-   export PATH=${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH
-   aqua i -l -a
+   # Install git-delta
+   mise use delta@latest
 fi
 
 # Change timezone
