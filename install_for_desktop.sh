1801d202062b281ff4a5ccfa24a1f3588ca4d869
Kenta Kase <kesin1202000@gmail.com>
2026-05-26 22:23:32 +0900

    git: Setup nested gitconfig for my org repos

--- install_for_desktop.sh
+++ install_for_desktop.sh
@@ -16,11 +16,12 @@ ln -is ~/dotfiles/.bashrc .
 ln -is ~/dotfiles/.vimrc .
 ln -is ~/dotfiles/.tmux.conf .
 ln -is ~/dotfiles/.inputrc .
-ln -is ~/dotfiles/.gitconfig .
 ln -is ~/dotfiles/.gitignore_global .
 ln -is ~/dotfiles/.dircolors .
 ln -is ~/dotfiles/.bash_aliases .
 ln -fs ~/dotfiles/aqua.yaml .
+
+cp -i ~/dotfiles/.gitconfig .
 
 if [ `uname` = "Darwin" ]; then
   # KeyRepeat settings
@@ -30,6 +31,13 @@ if [ `uname` = "Darwin" ]; then
   # Create .bashrc_local
   cp -i ~/dotfiles/.bashrc_mac ~/.bashrc_local
 
+  # Setup nested gitconfig
+  mkdir -p ~/dotfiles/com/kesin11
+  git config --global includeIf."gitdir:~/github/com/kesin11/**".path ~/dotfiles/.gitconfig_personal
+  mkdir -p ~/dotfiles/com/kesin11-private
+  git config --global includeIf."gitdir:~/github/com/kesin11-private/**".path ~/dotfiles/.gitconfig_personal
+  git config --global includeIf."gitdir:~/dotfiles/**".path ~/dotfiles/.gitconfig_personal
+
   # Install homebrew and bottles
   sh ~/dotfiles/brew-install.sh
 
@@ -38,8 +46,7 @@ if [ `uname` = "Darwin" ]; then
   chsh -s /opt/homebrew/bin/bash
 
 elif [ `uname` = "Linux" ]; then
-  ln -is ~/dotfiles/.bashrc_linux .bashrc_local
+  cp -i ~/dotfiles/.bashrc_linux .bashrc_local
 fi
 
 echo "-----Finish!!------"
-echo "Replace .gitconfig to local one if you need use another one"
